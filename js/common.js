b48c50c9c26ef134b7f31a54ae2c211237a57c3b
fantik <denib90185@gmail.com>
2018-04-02 11:20:00 +0300

    Edit project

--- js/common.js
+++ js/common.js
@@ -1,1 +1,62 @@
-$(document).ready(function(){$(".menu-icon").click(function(){$(".menu-icon").toggleClass("active")}),$(".menu-icon").click(function(){$(".nav-list").is(":visible")?($(".nav-list").fadeOut(600),$(".menu-icon").css("position","absolute"),$(".nav-list li").removeClass("fadeInUp animated")):($(".nav-list").fadeIn(600),$(".menu-icon").css("position","fixed"),$(".nav-list li").addClass("fadeInUp animated"))}),$(window).width()<769&&$(".nav-list li label").click(function(){$(".nav-list").fadeOut(600),$(".menu-icon").toggleClass("active")}),$("#tab-l1, #tab-l2, #tab-l3, #tab-l4, #tab-l5, #tab-l6, #tab-navl3").click(function(){$(".percent-block").countTo({speed:500,refreshInterval:30})}),$("#tab-navl2").click(function(){function n(n,a){return Math.random()*(a-n)+n}var a=$(".split"),i=new SplitText(a,{type:"words,chars"});i.chars,$(i.chars).each(function(a){TweenMax.from($(this),2.5,{opacity:0,x:n(-500,500),y:n(-500,500),z:n(-500,500),scale:.1,delay:.01*a,yoyo:!0})})})});
+$(document).ready(function() {
+// SANDWICH MENU
+  $('.menu-icon').click(function() {
+        $('.menu-icon').toggleClass('active');
+  });
+// NAVIGATION CONFIG
+    $('.menu-icon').click(function() {
+    if ($('.nav-list').is(':visible')) {
+      $('.nav-list').fadeOut(600);
+      $('.menu-icon').css('position', 'absolute');
+      $('.nav-list li').removeClass('fadeInUp animated');
+    } else {
+      $('.nav-list').fadeIn(600);
+      $('.menu-icon').css('position', 'fixed');
+      $('.nav-list li').addClass('fadeInUp animated');
+      
+      // $('.main-navigation li').addClass('fadeInLeft animated');
+    };
+  });
+
+
+  $('.nav-list li label').click(function() {
+    if (window.matchMedia("(max-width: 768px)").matches) {
+    $('.nav-list').fadeOut(600);
+    $('.menu-icon').toggleClass('active');
+    }
+  });
+
+
+//jQuery -countTo
+  $('#tab-l1, #tab-l2, #tab-l3, #tab-l4, #tab-l5, #tab-l6, #tab-navl3').click(function() {
+    $('.percent-block').countTo({speed:500,refreshInterval:30});
+  });
+
+// SplitText with TEamMAx!!!!
+  $('#tab-navl2').click(function(){
+    var text = $('.split');
+
+    var split = new SplitText(text, {type:'words,chars'}),
+        chars = split.chars;
+
+    function random(min, max){
+      return (Math.random() * (max - min)) + min;
+    }
+
+    $(split.chars).each(function(i){
+      TweenMax.from($(this), 2.5, {
+        opacity: 0,
+        x: random(-500, 500),
+        y: random(-500, 500),
+        z: random(-500, 500),
+        scale: .1,
+        delay: i * .01,
+        yoyo: true,
+
+      });
+    });
+  });
+
+
+});
+
